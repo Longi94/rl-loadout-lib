b8a869e6395835a148b5447f7652c8ebeb7c2d5c
Long Tran <lntrn94@gmail.com>
2019-10-11 12:36:06 +0200

    utils cleanup

--- src/utils/color.ts
+++ src/utils/color.ts
@@ -307,20 +307,6 @@ export function overBlendColors(foreground: Color, background: Color, foreground
   holder.setRGB(r, g, b);
 }
 
-/**
- * Get the text color for the background to make it readable.
- * https://www.w3.org/TR/AERT/#color-contrast
- *
- * @param backgroundColor color of background in #FFFFFF format
- */
-export function getTextColor(backgroundColor: Color) {
-  if (backgroundColor == undefined) {
-    return 'white';
-  }
-  const o = Math.round(((backgroundColor.r * 76245) + (backgroundColor.g * 149685) + (backgroundColor.b * 29070)) / 1000);
-  return (o > 125) ? 'black' : 'white';
-}
-
 export function getColorsForBody(body: Body): { [team: string]: string[] } {
   switch (body.id) {
     case BODY_MAPLE_ID:
