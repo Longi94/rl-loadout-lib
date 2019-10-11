60472ceccfdc229c0f7d237fe29ecedd863759f7
Long Tran <lntrn94@gmail.com>
2019-10-11 20:23:10 +0200

    provide GLTF loader from te outside to avoid issues with multiple webgl contexts

--- src/manager/rocket-asset-manager.ts
+++ src/manager/rocket-asset-manager.ts
@@ -1,32 +1,16 @@
 import { RocketLoadoutService } from '../service/rl-service';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { BodyModel } from '../3d/body/body-model';
 import { Decal } from '../model/decal';
 import { PaintConfig } from '../model/paint-config';
 import { WheelsModel } from '../3d/wheels-model';
 import { RocketConfig } from '../model/rocket-config';
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
 import { Body } from '../model/body';
 import { Wheel } from '../model/wheel';
 
 export class RocketAssetManager {
   private readonly rlService: RocketLoadoutService;
 
-  constructor(private readonly config?: RocketConfig) {
-    if (this.config == undefined) {
-      this.config = new RocketConfig();
-    }
-
-    if (this.config.gltfLoader == undefined) {
-      this.config.gltfLoader = new GLTFLoader();
-    }
-
-    if (this.config.dracoDecoderPath != undefined) {
-      const dracoLoader = new DRACOLoader();
-      dracoLoader.setDecoderPath(this.config.dracoDecoderPath);
-      this.config.gltfLoader.setDRACOLoader(dracoLoader);
-    }
-
+  constructor(private readonly config: RocketConfig) {
     this.rlService = new RocketLoadoutService(config.backendHost);
   }
 
--- src/model/rocket-config.ts
+++ src/model/rocket-config.ts
@@ -6,6 +6,11 @@ const DEFAULT_HOST = 'https://rocket-loadout.com';
 export class RocketConfig {
   backendHost: string = DEFAULT_HOST;
   assetHost: string = DEFAULT_BUCKET;
-  gltfLoader?: GLTFLoader = new GLTFLoader();
-  dracoDecoderPath?: string;
+  gltfLoader: GLTFLoader = new GLTFLoader();
+
+  constructor(gltfLoader: GLTFLoader, backendHost?: string, assetHost?: string) {
+    this.backendHost = backendHost == undefined ? DEFAULT_HOST : backendHost;
+    this.assetHost = assetHost == undefined ? DEFAULT_BUCKET : assetHost;
+    this.gltfLoader = gltfLoader;
+  }
 }
